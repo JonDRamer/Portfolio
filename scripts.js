9a54571424b1be31c4861a05202c8ba28dd8e163
Jon Ramer <jramer87@gmail.com>
2017-10-04 14:57:56 -0500

    Adds copy to clipboard functionality and tooltip to email link in footer

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$('document').ready((event) => {
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+      console.log("Tooltips enabled");
+  });
+
+  let clipboard = new Clipboard('.copyEmail');
+
+  clipboard.on('success', function(e) {
+      e.clearSelection();
+      $("#emailToolTip").tooltip({
+        title: "Email Copied",
+        placement: "top"
+      });
+      $("#emailToolTip").tooltip('show');
+      setTimeout(()=> {
+        $("#emailToolTip").tooltip('hide');
+      }, 1000)
+  });
+});
